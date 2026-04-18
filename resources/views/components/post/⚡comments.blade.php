93413c6e7a1ed0f4712d27c2ccb3ff66004b326d
Calebe Luvizotto <luvizott0@outlook.com>
2026-04-18 12:53:16 -0300

    rename component

--- resources/views/components/post/⚡comments.blade.php
+++ resources/views/components/post/⚡comments.blade.php
@@ -91,7 +91,7 @@ new class extends Component {
 
     <div class="flex items-start gap-2 mt-3">
         <div class="w-full">
-            <x-forms.input wire:model.live="comment" placeholder="{{ __('Deixe um comentário...') }}" max="500"/>
+            <x-forms.comment-field wire:model.live="comment" placeholder="{{ __('Deixe um comentário...') }}" max="500"/>
         </div>
 
         <button wire:click="send" class="bg-primary px-4 py-0.5 rounded-sm cursor-pointer">
